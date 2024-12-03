361385a31067d2dfd12f8cb8d7b5f8367fae47c3
denismusoev <denis.musoev@yandex.ru>
2024-12-04 00:09:48 +0400

    Update

--- src/main/java/com/coursework/springbootcomivoyadhor/model/Point.java
+++ src/main/java/com/coursework/springbootcomivoyadhor/model/Point.java
@@ -1,9 +1,4 @@
 package com.coursework.springbootcomivoyadhor.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
